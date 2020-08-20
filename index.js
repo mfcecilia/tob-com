13577c11c9d9d06b642fc37431a38963c4869254
mfcecilia <mkaykay1@vt.edu>
2020-08-20 11:05:15 -0700

    begin countdown function

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+//copyright
+
 function copyrightYear() {
      'use strict';
      document.getElementById("year").innerHTML = new Date().getFullYear();
  }
 
  copyrightYear(); 
+
+
+//countdown to burning man
+
+// Set the date we're counting down to
+var countDownDate = new Date("August 30, 2020 15:37:25").getTime();
+
+//countdown to end of burning man
